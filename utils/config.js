62b03546837475e550861419a4c3a3920194b324
Anni Heinonen <anni.heinonen@aalto.fi>
2021-09-23 10:36:38 +0300

    tehtavat 4.1 ja 4.2

---
+++ utils/config.js
@@ -0,0 +1,7 @@
+let PORT = 3003
+let MONGODB_URI = 'mongodb+srv://anni:kissa123@cluster0.rdszi.mongodb.net/blog-posts?retryWrites=true&w=majority'
+
+module.exports = {
+    MONGODB_URI,
+    PORT
+  }
