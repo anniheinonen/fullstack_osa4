b57b857edd14062b86079c1343fdf16f21c2a156
Anni Heinonen <anni.heinonen@aalto.fi>
2021-09-24 15:01:32 +0300

    tehtavat ->4.7

---
+++ utils/list_helper.js
@@ -0,0 +1,61 @@
+const _ = require("lodash");
+
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    return blogs.reduce(function (a, b) { return a + b.likes; }, 0);
+}
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return {}
+
+    const sorted = blogs.sort(function (a, b) {
+        return a.likes - b.likes;
+    });
+    return sorted[sorted.length - 1]
+
+}
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) return {}
+    const counted = _.map(_.countBy(blogs, "author"), (val, key) => ({ author: key, blogs: val }))
+    const sorted = counted.sort(function (a, b) {
+        return a.blogs - b.blogs;
+    });
+    return sorted[sorted.length - 1]
+}
+
+const mostLikes = (blogs) => {
+    if (blogs.length === 0) return {}
+
+    //const counted = _.groupBy(blogs, "author");
+    let counted = blogs.reduce((acc, curr) => {
+        let blog = acc.find(blog => blog.author === curr.author);
+
+        if (blog) {
+            blog.likes += curr.likes;
+        } else {
+            acc.push(curr);
+        }
+
+        return acc;
+    }, []);
+    const sorted = counted.sort(function (a, b) {
+        return a.likes - b.likes;
+    });
+
+    return {
+        "author": sorted[sorted.length -1].author,
+        "likes": sorted[sorted.length -1].likes
+    }
+}
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
+}
